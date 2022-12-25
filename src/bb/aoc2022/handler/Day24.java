75cb0ff2b614475270e7b56a91d9d8638e791d6e
Brett Benyo <brett.benyo@raytheon.com>
2022-12-24 20:32:59 -0500

    Day 24, part 1 complete

--- src/bb/aoc2022/handler/Day24.java
+++ src/bb/aoc2022/handler/Day24.java
@@ -1,7 +1,11 @@
 package bb.aoc2022.handler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
@@ -20,11 +24,11 @@ public class Day24 implements InputHandler {
 	Location bottomRight = null;
 	
 	protected class TimeState {
-		List<LocationFacing> blizzards;
+		Set<LocationFacing> blizzards;
 		int time;
 		
 		public TimeState(int t) {
-			blizzards = new ArrayList<>();
+			blizzards = new HashSet<>();
 			this.time = t;
 		}
 				
@@ -38,32 +42,56 @@ public class Day24 implements InputHandler {
 				bliz.forward(topLeft, bottomRight, true);
 			}
 		}
+		
+		public boolean isClear(Location l) {
+			int x = l.getX();
+			int y = l.getY();
+			for (LocationFacing lf : blizzards) {
+				if (lf.getX() == x && lf.getY() == y) {
+					return false;
+				}
+			}
+			return true;
+		}
 	}
+	
+	Map<Integer, TimeState> boardStates; // State of the board at time X
+	// If memory is an issue, we can save only some of these and compute the rest
+	
 	
 	// A*
 	class BNode extends Node {
 		
 		int time;
 
-		public BNode(Location l1) {
+		public BNode(Location l1, int time) {
 			super(l1);
+			this.time = time;
+		}
+		
+		@Override
+		public String toString() {
+			return super.toString()+" time: "+time;
 		}
 		
 		// Heuristic, guess on the risk score for the path to the end
 		//   We'll just do a right/down distance, and assume average risk of 5
+		@Override
 		protected void computeHScore() {
 			int dist = computeHeuristic();
-			int h = (dist * 5);
-			hScore = h;
+			int h = (dist * 2);
+			hScore = time + h;
 		}
 
 		// Compute the worst possible score from this node to initialize G
 		// To be overridden by implementations, this is a default
+		@Override
 		public int getWorstScore(Location l1) {
-			return getGridSizeX() * getGridSizeY(); // Assume we have to visit every square?
+			return time + (getGridSizeX() * getGridSizeY()); // Assume we have to visit every square, rough metric
 		}
 
 		// Return a heuristic to compute the likely score from this node
+		@Override
 		public int computeHeuristic() {
 			int right = getGridSizeX() - x;
 			int down = getGridSizeY() - y;
@@ -71,20 +99,106 @@ public class Day24 implements InputHandler {
 		}
 		
 		// Get the maximum X value for a Location
+		@Override
 		public int getGridSizeX() {
 			return grid.get(0).length - 1;
 		}
 		
 		// Get the maximum Y value for a Location
+		@Override
 		public int getGridSizeY() {
 			return grid.size() - 1;
 		}
 		
+		// Bottom right
+		@Override
+		public boolean isEnd(Location loc) {
+			if ((loc.getY() == getGridSizeY() - 1) &&
+				(loc.getX() == getGridSizeX() - 1)) {
+				return true;
+			}
+			return false;
+		}
+		
+		boolean isValidLocation(Location l) {
+			int x = l.getX();
+			int y = l.getY();
+			if (x == 1 && y == 0) {
+				return true;
+			}
+			if (x >= topLeft.getX() && x <= bottomRight.getX() &&
+				y >= topLeft.getY() && y <= bottomRight.getY()) {
+				return true;
+			}
+			return false;					
+		}
+				
+		// Add neighbors based on where we can go from here.
+		// Default implementation assumes a grid, and we can go up/down/left/right
+		@Override
+		public void gatherNeighbors() {
+			TimeState nextState = getBoardState(time + 1);
+			int x = getX() - 1;  //left
+			int y = getY();
+			Location lup = new Location(x, y);
+			addNeighbor(nextState, lup);
+			
+			x = getX();
+			y = getY() - 1; //up
+			lup = new Location(x, y);
+			addNeighbor(nextState, lup);
+						
+			x = getX() + 1;
+			y = getY(); // right
+			lup = new Location(x, y);
+			addNeighbor(nextState, lup);
+						
+			x = getX();
+			y = getY() + 1; // down
+			lup = new Location(x, y);
+			addNeighbor(nextState, lup);
+					
+			// Stay here
+			x = getX();
+			y = getY();
+			
+			addNeighbor(nextState, new Location(x,y));
+		}
+		
+		protected void addNeighbor(TimeState nextState, Location lup) {
+			if (!isValidLocation(lup)) {
+				return;
+			}
+			if (!nextState.isClear(lup)) {
+				return;
+			}
+			BNode node = new BNode(lup, time + 1);
+			this.neighbors.add(node);
+		}
+		
+	}
+	
+	protected TimeState getBoardState(int time) {
+		TimeState nextState = boardStates.get(time);
+		if (nextState == null) {
+			TimeState thisState = boardStates.get(time - 1);
+			if (thisState == null) {
+				// If we remove states from the cache (due to memory limitations), we can recompute here
+				// Just go back as far as we need to, and propagate forward
+				// For now, we assume that we won't need to remove any
+				throw new RuntimeException("TimeState for "+(time-1)+" is missing!");
+			}
+			nextState = new TimeState(time + 1);
+			nextState.propagateForward(thisState);
+			boardStates.put(time, nextState);
+		}
+		return nextState;
 	}
 	
 	@Override
 	public void initialize() {
 		grid = new ArrayList<>();
+		boardStates = new HashMap<>();
 	}
 
 	@Override
@@ -172,10 +286,16 @@ public class Day24 implements InputHandler {
 	public void output() {
 		computeBoundingBox();
 		TimeState t0 = createBlizzards();
+		boardStates.put(0, t0);
 		drawGrid(t0);
-		TimeState t1 = new TimeState(1);
-		t1.propagateForward(t0);
-		drawGrid(t1);
+		
+		BNode startNode = new BNode(new Location(1,0), 0);
+		Node end = Node.search(startNode);
+		BNode endNode = (BNode)end;		
+		TimeState tEnd = this.getBoardState(endNode.time);
+		drawGrid(tEnd);
+		logger.info("End Time - 1: "+endNode.time);
+		logger.info("Move down 1, endTime: "+(endNode.time + 1));
 	}
 
 }
